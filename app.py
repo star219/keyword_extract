89b7bf8f8d73adee9009c4b6f89609a0be504bc6
developer <codedreams9873@gmail.com>
2022-11-15 17:44:38 +0800

    change

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 from flask import Flask, render_template, Response, request, jsonify, make_response
-
+from rake_nltk import Rake
+rake_nltk_var = Rake()
 
 app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/form_action', methods=["POST"])
-def form_action():
-	print(request.form['username'])
-	print(request.form['password'])
-	return render_template('success.html')
-
 @app.route('/ajax_action',methods=["POST"])
 def ajax_action():
 	print("djhghj")
-	print(request.get_json())
-	return "success"
+	sentenceTxt = request.get_json()
+	text = sentenceTxt['name']
+	rake_nltk_var.extract_keywords_from_text(text)
+	keyword_extracted = rake_nltk_var.get_ranked_phrases()
+	return keyword_extracted
 
 @app.route('/success')
 def success():
 	return render_template('success.html')
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0",debug=True,threaded=True)
+	app.run(host="127.0.0.1",debug=True,threaded=True)
